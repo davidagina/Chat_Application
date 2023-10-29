488aeb9def4caba91acb34eddca5a20e916a5db1
davidagina <davidagina64@gmail.com>
2023-10-29 21:43:47 +0100

    Updated

--- src/main/java/com/daveproject/chatapp/config/WebSocketEventListener.java
+++ src/main/java/com/daveproject/chatapp/config/WebSocketEventListener.java
@@ -1,4 +1,20 @@
 package com.daveproject.chatapp.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        // Todo to be implement
+    }
+
+
 }
