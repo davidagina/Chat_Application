413eb543cb6ac573278578795c72dbdb34539abe
davidagina <davidagina64@gmail.com>
2023-10-30 21:07:32 +0100

    Added sendMessage and addUser methods

--- src/main/java/com/daveproject/chatapp/controller/ChatController.java
+++ src/main/java/com/daveproject/chatapp/controller/ChatController.java
@@ -1,9 +1,25 @@
 package com.daveproject.chatapp.controller;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ChatController {
 
+        @MessageMapping("/chat.sendMessage")
+        @SendTo("/topic/public")
+        public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+                return chatMessage;
+        }
 
+        public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                                   SimpMessageHeaderAccessor headerAccessor){
+                // Add username to websocket
+                headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+                return chatMessage;
+
+        }
 }
