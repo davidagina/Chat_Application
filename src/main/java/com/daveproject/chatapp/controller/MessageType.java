ea5bdb001ce531ebe07f8fc4256b032cce3ab8bc
davidagina <davidagina64@gmail.com>
2023-10-30 21:05:45 +0100

    Added MessageType class

---
+++ src/main/java/com/daveproject/chatapp/controller/MessageType.java
@@ -0,0 +1,13 @@
+package com.daveproject.chatapp.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
