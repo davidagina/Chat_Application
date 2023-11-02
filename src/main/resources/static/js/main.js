39a4753b2720f2ee9c33acb5904f16d0e6dda1a1
davidagina <davidagina64@gmail.com>
2023-11-02 08:04:27 +0100

    Updated Stylesheet

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var usernamePage = document.querySelector("#username-page");
+var chatPage = document.querySelector("#chat-page");
+var usernameForm = document.querySelector('#usernameForm');
+var messageForm = document.querySelector('#messageForm');
+var messageInput = document.querySelector('#message');
+var messageArea = document.querySelector('#messageArea');
+var connectingElement = document.querySelector('.connecting');
+
+
